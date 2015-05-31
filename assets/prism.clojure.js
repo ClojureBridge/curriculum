a6edda4b61773cca2e461881843acbd99d46d3b1
Yoko Harada <yokolet@gmail.com>
2015-05-31 14:18:47 -0400

    Imporves clojure specific code highlighting

--- assets/prism.clojure.js
+++ assets/prism.clojure.js
@@ -1,8 +1,10 @@
 Prism.languages.clojure = {
   comment: /;.*/g,
   string: /"(?:\\"|[^"])*"/g,
-  keyword: /:[a-z][-a-z0-9?]*/gi,
+  symbol: /:[a-z][-a-z0-9?]*/gi,
   boolean: /(^|[^-])(true|false)/g,
   number: /-?\b\d+([./]\d+)?\b/g,
-  punctuation: /[[\](){},]/g
+punctuation: /[[\](){},]/g,
+operator: /(\*|\+|\-\s|%|\/|<=|=>|>=|<|=|>)/,
+keyword: /\b(apply|and|assoc|case|count|lower-case|upper-case|catch|class|conj|cons|const|def|defn|dissoc|do|else|enum|export|extends|finally|first|fn|for|get|if|implements|import|instanceof|interface|keys|last|let|list|map|max|merge|new|nil|not|not=|nth|or|private|reduce|rest|return|set|static|super|switch|throw|try|vals|var|vector|while|yield)\b/,
 };
